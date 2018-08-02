37589dd1b819b64d7e3ff72a09adae5bb19613d9
kirichenko <kirichenko@SPBUNIC2160AH97.spb.unicum.ru>
2018-08-02 17:18:01 +0300

    Промежуточный коммит, скомпоновать все элементы интерфейса

--- PDF-Split-&-Merge/src/Main.java
+++ PDF-Split-&-Merge/src/Main.java
@@ -1,10 +1,3 @@
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-
-import drawing.Image;
-
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -14,14 +7,10 @@ public class Main {
 	private void run() {
 		String url = "/pdf-document.png";
 
-		JFrame frame = new JFrame();
-		frame.setSize(new Dimension(640, 480));
-		Canvas canvas = new Canvas();
-		canvas.setSize(new Dimension(640, 480));
-		canvas.addDrawObject(new Image(10, 10, 170, 200, url));
-		frame.add(canvas);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		Application app = new Application();
+		app.addPage(new PageFrame(1, url));
+		app.drawPageFrames();
+		app.start();
 	}
 
 }
